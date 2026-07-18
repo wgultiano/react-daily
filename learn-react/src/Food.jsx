2c177998f0906bea680f746dcf3e895e27ec66df
Winston Gultiano <wgultiano08@gmail.com>
2026-07-18 22:29:45 +0800

    Day 1 of learning React

---
+++ learn-react/src/Food.jsx
@@ -0,0 +1,16 @@
+
+function Food() {
+
+    const food1 = "Banana";
+    const food2 = "Cherry";
+
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    )
+}
+
+export default Food
---
+++ learn-react/src/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    )
+}
+
+export default Header
